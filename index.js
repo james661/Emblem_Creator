3ba257904ede8f9deb696bca5012569df3af16f0
A Friend <jamesshami13@gmail.com>
2023-06-22 19:07:27 -0700

    Added comments to files

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
+// Importing all necessary modules and the shape classes.
 const { Circle, Square, Triangle } = require("./lib/shapes");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+//
 class Svg {
   constructor() {
     this.textElement = "";
     this.shapeElement = "";
   }
-
+  // Renders the SVG with the chosen shape and text. Xmlns provides rendering on simple browsers.
   render() {
-    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-      ${this.shapeElement}${this.textElement}
-    </svg>`;
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.shapeElement}${this.textElement}</svg>`;
   }
 
   setText(text, color) {
-    this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">
-      ${text}
-    </text>`;
+    this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
   }
 
   setShape(shape) {
     this.shapeElement = shape.render();
   }
 }
-
+// Function to write data to file.
 function writeToFile(fileName, data) {
   fs.writeFile(fileName, data, (err) => {
     if (err) {
@@ -34,8 +32,9 @@ function writeToFile(fileName, data) {
     }
   });
 }
-
+// Function to initialize in Node.
 async function init() {
+  // Asks the user 4 questions and collects their answers.
   const questions = [
     {
       name: "text",
@@ -59,9 +58,9 @@ async function init() {
       message: "Choose a shape color by name or with a hexadecimal number",
     },
   ];
-
+  // Create a new SVG constructor so we can add text and shape elements.
   const svg = new Svg();
-
+  // Puts the user's answers in variables to be used later.
   try {
     const answers = await inquirer.prompt(questions);
     const userText = answers.text;
@@ -70,7 +69,7 @@ async function init() {
     const userShapeType = answers.logoShape;
 
     let userShape;
-
+    // Switch case for each shape choice possible. Creates a new shape constructor.
     switch (userShapeType) {
       case "Square":
         userShape = new Square();
@@ -81,13 +80,10 @@ async function init() {
       case "Triangle":
         userShape = new Triangle();
         break;
-      default:
-        console.log("Please choose another shape");
-        return;
     }
 
     userShape.setColor(userShapeColor);
-
+    // Adds the text and shape elements to the new SVG instance.
     svg.setText(userText, userFontColor);
     svg.setShape(userShape);
     const svgString = svg.render();
@@ -98,4 +94,5 @@ async function init() {
   }
 }
 
+// Call the initializing function
 init();
